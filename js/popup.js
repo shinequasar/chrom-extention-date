0cc2b72e19ef549ddbcb043bac3a1db4f5f745be
shinequasar <shinequasar@gmail.com>
2021-09-02 01:45:56 +0900

    [add]기본 디자인 html, css 골격 완성

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,4 @@
-
+function calculateDate(){
     var startDate = new Date('29/08/2020 00:00:00');
     var nowDate = new Date();
     var dDay = nowDate - startDate;
@@ -6,3 +6,5 @@
     var esDay = d;
     document.getelementbyid('title').innerText = "dmmm";
     // document.body.innerText = nowTime;
+}
+window.onload = calculateDate;

1b803e24f7faa6bf8c1db125ed03afed5213c56a
shinequasar <shinequasar@gmail.com>
2021-09-02 00:39:19 +0900

    [feat] html 에서 html 불러오기(includeHTML)

---
+++ js/popup.js
@@ -0,0 +1,8 @@
+
+    var startDate = new Date('29/08/2020 00:00:00');
+    var nowDate = new Date();
+    var dDay = nowDate - startDate;
+
+    var esDay = d;
+    document.getelementbyid('title').innerText = "dmmm";
+    // document.body.innerText = nowTime;
